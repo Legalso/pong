6be8c05a7b86958ebd364777b58794f8bc8584a7
caiopatriota <caiopatriotabarros@gmail.com>
2022-12-07 23:12:24 -0300

    Pong v2.0
    Primeira atualização!!!
    + Placar de jogo;
    + Sistema de pontuações;
    + Sons;
    + Alteração na cor das raquetes para verde;
    + Alteração na cor da bolinha para verde;
    - Bug da bolinha ficar presa removido;

--- js/pong.js
+++ js/pong.js
@@ -1,32 +1,45 @@
-//variáveis da bolinha
-let xBolinha = 300;
-let yBolinha = 200;
-let diametro = 25;
-let raio = diametro / 2 ;
-
-//velocidade da bolinha
-let velocidadeXBolinha = 5;
-let velocidadeYBolinha = 5;
-let raqueteComprimento = 10;
-let raqueteAltura = 90;
-
-//variáveis da raquete
-let xRaquete = 6;
-let yRaquete = 150;
-
-//variáveis do oponente
-let xRaqueteOponente = 588;
-let yRaqueteOponente = 150;
-let velocidadeYOponente;
-
-let colidiu = false;
-
 //placar do jogo
 let meusPontos = 0;
 let pontosDoOponente = 0;
 
+//variáveis da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 30;
+let raio = diametro / 2 ;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 155;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 155;
+let velocidadeYOponente;
+let chanceDeErrar = 0;
+
+let colidiu = false;
+
+// variáveis de som
+let trilha;
+let ponto;
+let raquetada;
+
+function preload(){
+  trilha = loadSound("trilha.mp3");
+  ponto = loadSound ("ponto.mp3");
+  raquetada = loadSound("raquetada.mp3");
+}
+
 function setup() {
   createCanvas(600, 400);
+  trilha.loop();
 }
 
 function draw() {
@@ -36,11 +49,14 @@ function draw() {
   verificaColisaoBorda();
   mostraRaquete(xRaquete, yRaquete);
   movimentaMinhaRaquete();
-  //verificaColisaoRaquete();
+  //verificaColisaoRaquete2();
   verificaColisaoRaquete(xRaquete, yRaquete);
   mostraRaquete(xRaqueteOponente, yRaqueteOponente);
   movimentaRaqueteOponente();
   verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
+  incluiPlacar();
+  marcaPonto();
+  bolinhaNaoFicaPresa()
 }
 
 function mostraBolinha(){
@@ -77,7 +93,7 @@ function movimentaMinhaRaquete(){
 }
 
 /*função não utilizada, substituída pela da biblioteca p5.collide2d
-function verificaColisaoRaquete() { 
+function verificaColisaoRaquete2() { 
   if (xBolinha - raio < xRaquete + raqueteComprimento && yBolinha - raio < yRaquete + raqueteAltura && yBolinha + raio > yRaquete) {
     velocidadeXBolinha *= -1;
   }
@@ -87,10 +103,64 @@ function verificaColisaoRaquete(x, y){
   colidiu = collideRectCircle(x, y,raqueteComprimento,raqueteAltura, xBolinha,yBolinha,raio);
   if (colidiu){
     velocidadeXBolinha *= -1;
+    raquetada.play();
   }
 }
 
-function movimentaRaqueteOponente(){
-  velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento / 2 - 30;
-  yRaqueteOponente += velocidadeYOponente
+/*function movimentaRaqueteOponente(){
+  velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento + 30 + chanceDeErrar;
+  yRaqueteOponente += velocidadeYOponente;
+  calculaChanceDeErrar();
 }
+
+function calculaChanceDeErrar(){
+  if (pontosDoOponente > meusPontos + 1){
+    chanceDeErrar = random (-54, 50)
+  }
+  else {
+    chanceDeErrar = 0
+  }
+}
+*/
+
+function movimentaRaqueteOponente(){
+  velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteAltura + 30 + chanceDeErrar;
+  yRaqueteOponente += velocidadeYOponente;
+  calculaChanceDeErrar();
+}
+
+function calculaChanceDeErrar(){
+  if (pontosDoOponente > meusPontos + 1) {
+    chanceDeErrar = random (-54, -50)}
+  else {chanceDeErrar = 21}
+}
+
+function incluiPlacar(){
+  textStyle(BOLD);
+  textAlign (CENTER);
+  fill (color(0, 255, 0))
+  textSize (45);
+  text(meusPontos, 250, 45);
+  text(pontosDoOponente, 350, 45);
+}
+
+function marcaPonto(){
+  if (xBolinha > 585){
+    meusPontos += 1;
+    ponto.play();
+  }
+  if (xBolinha < 15){
+    pontosDoOponente += 1;
+    ponto.play();
+  }
+}
+
+function bolinhaNaoFicaPresa(){
+    if (xBolinha - raio < 0){
+      xBolinha = 23
+    }
+    if (xBolinha + raio > 600){
+      xBolinha = 580
+    }
+}
+
